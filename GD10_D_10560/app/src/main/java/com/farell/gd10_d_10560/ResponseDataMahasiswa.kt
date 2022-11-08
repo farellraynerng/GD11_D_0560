91fa78f96456f5cc43866b86f6ad2be74258aaa1
farellrayner <farellrayner@gmail.com>
2022-11-08 10:57:06 +0700

    GD10_D_10560_AndroidStudio

---
+++ GD10_D_10560/app/src/main/java/com/farell/gd10_d_10560/ResponseDataMahasiswa.kt
@@ -0,0 +1,8 @@
+package com.farell.gd10_d_10560
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>)
